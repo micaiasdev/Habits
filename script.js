6054b3506d15a007a0f238bf72ec32e09fecacce
shiftdev <miciaiasvieira@gmail.com>
2023-01-20 00:34:23 -0300

    add-days-in-libray-javascript

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-	run: "04-02",
+	run: ["04-02", "05-02", "08-02"],
+	water: ["04-02", "06-02", "08-02"],
+	work: ["01-02", "02-02", "03-02", "04-02", "05-02"],
 }
+
+nlwSetup.setData(data)
+nlwSetup.load()
