cfc8492fb28666efcf850498b3f88859c254be5e
shiftdev <miciaiasvieira@gmail.com>
2023-01-20 02:03:30 -0300

    add-function-eventListener-in-button-forAdd-date

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("button")
 
-const data = {
+button.addEventListener("click", add)
+
+function add() {
+	nlwSetup.addDay("01/02")
+}
+
+/* const data = {
 	run: ["04-02", "05-02", "08-02"],
 	water: ["04-02", "06-02", "08-02"],
 	work: ["01-02", "02-02", "03-02", "04-02", "05-02"],
-}
+} 
 
 nlwSetup.setData(data)
-nlwSetup.load()
+nlwSetup.load()*/
